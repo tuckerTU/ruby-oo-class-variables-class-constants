b842bde484bb6ebbeda7fdcb6ae012cfd4c7d218
tuckerTU <tuckerishere@gmail.com>
2020-11-06 12:01:03 -0700

    commit tree e4aba8a2ff3d1420de232d0a5eac04749a023a06

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,14 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+  
+    if !BRANDS.include?(brand)
+      BRANDS << brand 
+    end
   end
 
   def cobble
@@ -11,4 +17,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-end
+end 
